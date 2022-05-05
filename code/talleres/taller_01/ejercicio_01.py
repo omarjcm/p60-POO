d15c914476017326eeef8d119b03d30542c2bff8
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2022-05-05 10:35:13 -0500

    Creacion de la primera lista

---
+++ code/talleres/taller_01/ejercicio_01.py
@@ -0,0 +1,5 @@
+nombres = ['Guillermo', 'Keneth', 'Elizabeth']
+apellidos = ['Pizarro', 'Vera', 'Martinez']
+manzana = [1, 1, 3]
+villa =[2, 5, 1]
+fecha_inicio = ['05-05-2022', '05-05-2020', '05-07-2020']
