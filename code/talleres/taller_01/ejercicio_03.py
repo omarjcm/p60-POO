3661d0b77490e5a6dba144f8a82d44fa8e45ba36
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2022-05-05 10:52:56 -0500

    Modificacion y Eliminacion de valores

--- code/talleres/taller_01/ejercicio_03.py
+++ code/talleres/taller_01/ejercicio_03.py
@@ -7,5 +7,12 @@ residentes = [
 residentes.append( [4, 'Steven', 'Baque', 2, 3, '05-07-2019'] )
 residentes.append( [len(residentes)+1, 'Alexander', 'Leon', 2, 4, '05-07-2021'] )
 
+# Modificar el primer elemento
+#residentes[0] = [1, 'Alexander', 'Leon', 2, 4, '05-07-2021']
+#residentes[0][1] = 'Leon Laborda'
+
+# Elimimar el primer elemento
+#del residentes[0]
+
 for i in range(0, len(residentes)):
     print( residentes[i] )
