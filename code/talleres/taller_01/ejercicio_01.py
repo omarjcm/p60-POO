06a2c68fc18bbea98efb7348af329d927d83f625
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2022-05-05 10:39:09 -0500

    Recorrido de la lista

--- code/talleres/taller_01/ejercicio_01.py
+++ code/talleres/taller_01/ejercicio_01.py
@@ -1,5 +1,10 @@
+# Primera forma de crear mi estructura de datos
+
 nombres = ['Guillermo', 'Keneth', 'Elizabeth']
 apellidos = ['Pizarro', 'Vera', 'Martinez']
 manzana = [1, 1, 3]
 villa =[2, 5, 1]
 fecha_inicio = ['05-05-2022', '05-05-2020', '05-07-2020']
+
+for i in range(0, 3):
+    print('{} {}'.format(nombres[i], apellidos[i]))
