b5be5e23671c0bdf0e641f41a057d53be71d5853
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2022-05-10 09:53:11 -0500

    Ingresar valores

---
+++ code/talleres/taller_01/ejercicio_05.py
@@ -0,0 +1,7 @@
+nombre = input('Ingresar su nombre: ')
+print('Hola ' + nombre)
+
+primer_valor = input('Ingresar primer numero: ')
+segundo_valor = input('Ingresar segundo numero: ')
+resultado = int(primer_valor) + int(segundo_valor)
+print(resultado)
