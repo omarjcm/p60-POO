807881a43d9a34a25febcda90e1bff9053ce9592
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2022-05-26 10:09:43 -0500

    Agregar paciente para consulta

--- code/talleres/02_clinica/clinica/Consulta.py
+++ code/talleres/02_clinica/clinica/Consulta.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 class Consulta:
     def __init__(self, fecha_consulta, paciente, especialidad):
-        self.__fecha_consulta = datetime.strptime(fecha_consulta, '%d-%m-%Y')
+        self.__fecha_consulta = fecha_consulta
         self.__valor_descuento = 0.0
         self.__valor_total = 0.0
         self.ref_paciente = paciente
@@ -19,4 +19,4 @@ class Consulta:
         self.__fecha_consulta = datetime.strptime(valor, '%d-%m-%Y')
 
     def __str__(self):
-        return str(self.__fecha_consulta.day) + '/' + str(self.__fecha_consulta.month) + '/' + str(self.__fecha_consulta.year) + ' ' + str(self.ref_paciente) + ' - ' + str(self.ref_especialidad)
+        return str(self.ref_paciente) + ' - ' + str(self.ref_especialidad)
--- code/talleres/02_clinica/clinica/app.py
+++ code/talleres/02_clinica/clinica/app.py
@@ -1,6 +1,7 @@
 from Paciente import *
 from Consulta import *
 from Especialidad import *
+from datetime import datetime
 
 especialidades = [
     Especialidad('Medicina General', 30),
@@ -32,5 +33,42 @@ consultas = [
     Consulta('10-05-2022', pacientes[7], especialidades[1]),
 ]
 
-for consulta in consultas:
-    print(consulta)
+def menu_principal():
+    print('Sistema de la Clinica UPS')
+
+    opcion = 'N'
+    while opcion == 'N':
+        opcion = input('¿Desea ingresar un nuevo paciente? [S-N]: ')
+        if opcion == 'N':
+            break
+
+        nombre = input('Nombre del Paciente: ')
+        apellido = input('Apellido del Paciente: ')
+        sexo = input('Sexo [M-F]: ')
+        anio_nacimiento = int(input('Año de Nacimiento: '))
+        paciente = Paciente(nombre, apellido, sexo, anio_nacimiento)
+        
+        menu_especialidades()
+        subopcion = int(input('Especialidad [1-2-3-4-5]: '))
+        especialidad = None
+        if (subopcion >=0 and subopcion <=5):
+            especialidad = especialidades[subopcion-1]
+        else:
+            print('No ingresa de manera correcta la especialidad')
+        
+        fecha_actual = datetime.now()
+        consulta = Consulta(fecha_actual, paciente, especialidad)
+        consultas.append(consulta)
+        
+        opcion = input('¿Desea salir? [S-N]')
+
+def menu_especialidades():
+    print('Seleccionar la especialidad: ')
+    print('1 - Medicina General - $30')
+    print('2 - Cardiologia - $35')
+    print('3 - Traumatologia - $50')
+    print('4 - Dermatologia - $45')
+    print('5 - Pediatria - $40')
+
+if __name__ == '__main__':
+    menu_principal()
