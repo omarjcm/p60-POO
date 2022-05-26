d86a010379d61c8b9a3ad313b8477f36508d7b97
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2022-05-26 13:21:14 -0500

    Eliminacion de archivos de pruebas.

--- code/talleres/02_clinica/Model/Especialidad.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-#-*- coding: utf-8 -*-
-
-class Especialidad:
-    def __init__(self, nombre, valor):
-        self.nombre = nombre
-        self.valor = valor
-
--- code/talleres/02_clinica/Model/Generar_Consulta.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-#-*- coding: utf-8 -*-
-
-class Generar_Consulta:
-    def __init__(self, fecha_consulta, paciente, especialidad):
-        self.fecha_consulta = fecha_consulta
-        self.ref_paciente = paciente
-        self.ref_especialidad = especialidad
-
