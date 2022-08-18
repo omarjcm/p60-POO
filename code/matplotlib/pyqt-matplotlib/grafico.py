848106bf6fdea8134da1f0aec8a01c8c9448396d
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2022-08-18 10:13:44 -0500

    Agregar n[umeros aleatorios

--- code/matplotlib/pyqt-matplotlib/grafico.py
+++ code/matplotlib/pyqt-matplotlib/grafico.py
@@ -6,8 +6,32 @@ class Grafico(QtWidgets.QMainWindow, Ui_MainWindow):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)
 
+        indice = 0
         numeros = np.random.randint(100, size=(12))
-        self.enero_txt.setText(str(numeros[0]))
+        self.enero_txt.setText(str(numeros[indice]))
+        indice += 1
+        self.febrero_txt.setText(str(numeros[indice]))
+        indice += 1
+        self.marzo_txt.setText(str(numeros[indice]))
+        indice += 1
+        self.abril_txt.setText(str(numeros[indice]))
+        indice += 1
+        self.mayo_txt.setText(str(numeros[indice]))
+        indice += 1
+        self.junio_txt.setText(str(numeros[indice]))
+        indice += 1
+        self.julio_txt.setText(str(numeros[indice]))
+        indice += 1
+        self.agosto_txt.setText(str(numeros[indice]))
+        indice += 1
+        self.septiembre_txt.setText(str(numeros[indice]))
+        indice += 1
+        self.octubre_txt.setText(str(numeros[indice]))
+        indice += 1
+        self.noviembre_txt.setText(str(numeros[indice]))
+        indice += 1
+        self.diciembre_txt.setText(str(numeros[indice]))
+        indice += 1
 
         self.generar_grafico_btn.clicked.connect( self.generar_grafico )
 
