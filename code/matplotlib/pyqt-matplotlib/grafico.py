accc4ceef87a94e4f9515d2ab9ffa55ddcc3d0e4
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2022-08-18 10:28:35 -0500

    Generar grafico

--- code/matplotlib/pyqt-matplotlib/grafico.py
+++ code/matplotlib/pyqt-matplotlib/grafico.py
@@ -1,11 +1,16 @@
+from turtle import width
 from interfaz_ui import *
 import numpy as np
+import matplotlib.ticker as ticker
 
 class Grafico(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)
+        self.generar_numeros_aleatorios()
+        self.generar_grafico_btn.clicked.connect( self.generar_grafico )
 
+    def generar_numeros_aleatorios(self):
         indice = 0
         numeros = np.random.randint(100, size=(12))
         self.enero_txt.setText(str(numeros[indice]))
@@ -33,10 +38,32 @@ class Grafico(QtWidgets.QMainWindow, Ui_MainWindow):
         self.diciembre_txt.setText(str(numeros[indice]))
         indice += 1
 
-        self.generar_grafico_btn.clicked.connect( self.generar_grafico )
-
     def generar_grafico(self):
-        pass
+        meses = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic']
+        x = [i for i in range(12)]
+
+        data = []
+        data.append( int(self.enero_txt.text()) )
+        data.append( int(self.febrero_txt.text()) )
+        data.append( int(self.marzo_txt.text()) )
+        data.append( int(self.abril_txt.text()) )
+        data.append( int(self.mayo_txt.text()) )
+        data.append( int(self.junio_txt.text()) )
+        data.append( int(self.julio_txt.text()) )
+        data.append( int(self.agosto_txt.text()) )
+        data.append( int(self.septiembre_txt.text()) )
+        data.append( int(self.octubre_txt.text()) )
+        data.append( int(self.noviembre_txt.text()) )
+        data.append( int(self.diciembre_txt.text()) )
+
+        self.widget.canvas.ax.clear()
+        self.widget.canvas.ax.axis( [-0.5, 11.5, 0, max(data)+10] )
+        self.widget.canvas.ax.xaxis.set_major_locator(ticker.FixedLocator((x)))
+        self.widget.canvas.ax.xaxis.set_major_formatter(ticker.FixedFormatter((meses)))
+        self.widget.canvas.ax.bar(x, data, align='center', width=1)
+        self.widget.canvas.draw()
+
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
